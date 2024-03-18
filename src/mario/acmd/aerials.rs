ddffde0db2ca87380eab0e52333251ac13e936ec
ACKREIK <63441042+ACKREIK@users.noreply.github.com>
2024-03-18 13:03:33 -0700

    Readd mario scripts for smashline 2, setup automatic building

--- src/mario/acmd/aerials.rs
+++ src/mario/acmd/aerials.rs
@@ -15,10 +15,10 @@ unsafe extern "C" fn mario_attackairf(fighter: &mut L2CAgentBase) {
     }
     frame(fighter.lua_state_agent, 16.0);
     if macros::is_excute(fighter) {
-        macros::ATTACK(fighter, 0, 0, Hash40::new("arml"), 19.0 /* Damage, changed from 12.0 */, 361, 80, 0, 30, 113.0 /* Size, changed from 3.0 */, 3.2, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_PUNCH, *ATTACK_REGION_PUNCH);
+        macros::ATTACK(fighter, 0, 0, Hash40::new("arml"), 55.0 /* Damage, changed from 12.0 */, 361, 80, 0, 30, 113.0 /* Size, changed from 3.0 */, 3.2, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_PUNCH, *ATTACK_REGION_PUNCH);
     }
     // Also, the hitbox doesn't clear until the action ends.
-    frame(fighter.lua_state_agent, 43.0);
+    frame(fighter.lua_state_agent, 10.0);
     if macros::is_excute(fighter) {
         WorkModule::off_flag(fighter.module_accessor, *FIGHTER_STATUS_ATTACK_AIR_FLAG_ENABLE_LANDING);
     }
