bc934c7d39fa4c2a24e47ed4a8c0c7fe2bcc3932
ACKREIK <63441042+ACKREIK@users.noreply.github.com>
2024-03-18 18:54:07 -0700

    Update Readme and fix UPB

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CDMod Source (Name Temporary)
 
+[![Github](https://img.shields.io/github/v/release/Nadia8666/CRMod?include_prereleases)](https://github.com/Nadia8666/CRMod/releases/latest) [![Downloads](https://img.shields.io/github/downloads/Nadia8666/CRMod/total.svg)](https://github.com/Nadia8666/CRMod/releases/latest)
+
 TBD.
--- src/mario/acmd/aerials.rs
+++ src/mario/acmd/aerials.rs
@@ -12,78 +12,61 @@ use {
 
 
 unsafe extern "C" fn game_specialhi(fighter: &mut L2CAgentBase) {
-    WorkModule::on_flag(fighter.module_accessor, *FIGHTER_STATUS_SUPER_JUMP_PUNCH_FLAG_REVERSE_LR);
-    macros::SA_SET(fighter, *SITUATION_KIND_AIR);
-
-    macros::ATTACK(
-        fighter,
-        0, // id
-        0, // part
-        Hash40::new("hip"), // bone
-        58.0, // dmg
-        90, // ang
-        200, // kbg
-        0, // fkb
-        200, // bkb
-        15.0, // size
-        0.0, // hitboxstart X
-        6.0, // hitboxstart Y
-        0.0, // hitboxstart Z
-        Some(0.0), // hitboxend X
-        Some(10.0), // hitboxend Y
-        Some(0.0),  // hitboxend Z
-        1.0, // hitlag
-        1.0, // sdi
-        *ATTACK_SETOFF_KIND_OFF, // ignore
-        *ATTACK_LR_CHECK_POS,  // ignore
-        false,  // knockback affected by weight
-        3, // extra dmg to shield 2.1 = all
-        0.0, // trip chance
-        0, // rehit
-        false, // reflectable
-        false, // absorbable
-        false, // flinchless
-        false, // disable hitlag
-        true, // direct hitbox (hitbox from the fighter, false if its from a weapon or smth)
-        *COLLISION_SITUATION_MASK_GA, // ground or air only, G = GROUND | A = AIR | GA = BOTH
-        *COLLISION_CATEGORY_MASK_ALL, // ignore
-        *COLLISION_PART_MASK_ALL, // ignore
-        false,  // friendly fire
-        Hash40::new("collision_attr_normal"), // effect https://raw.githubusercontent.com/ultimate-research/param-labels/master/ParamLabels.csv collision_attr
-        *ATTACK_SOUND_LEVEL_L, // sfx vol S/M/L
-        *COLLISION_SOUND_ATTR_KICK, // sfx type ignore most time
-        *ATTACK_REGION_THROW // hitbox type ignore
-    );
-
-
-    // Send player high up over time
-    for _ in 0..10 {
-        if macros::is_excute(fighter) {
-            //WorkModule::on_flag(fighter.module_accessor, *FIGHTER_STATUS_SUPER_JUMP_PUNCH_FLAG_MOVE_TRANS);
-        }
-        wait(fighter.lua_state_agent, 10.0);
+    frame(fighter.lua_state_agent, 1.0);
+    macros::FT_MOTION_RATE(fighter, 3.0);
+    frame(fighter.lua_state_agent, 3.0);
+    macros::FT_MOTION_RATE(fighter, 1.0);
+    let mut effect = Hash40::new("collision_attr_coin");
+    let mut sound = *COLLISION_SOUND_ATTR_COIN;
+    if WorkModule::is_flag(fighter.module_accessor, *FIGHTER_MARIO_STATUS_SPECIAL_HI_FLAG_CAPPY) {
+        effect.hash = Hash40::new("collision_attr_mario_local_coin").hash;
+        sound = *COLLISION_SOUND_ATTR_MARIO_LOCAL_COIN;
     }
+    
 
-    // Transition
-    wait(fighter.lua_state_agent, 10.0);
-
-    // Set baack to fall state
     if macros::is_excute(fighter) {
-        // Give back jumps
-        WorkModule::set_int(fighter.module_accessor, 0, *FIGHTER_INSTANCE_WORK_ID_INT_JUMP_COUNT);
-
-        // Do jump effects
-        macros::EFFECT_FOLLOW(fighter, Hash40::new("sys_jump_smoke"), Hash40::new("top"), 0, 0, 0, 0, 0, 0, 1, false);
-        WorkModule::is_flag(fighter.module_accessor, *FIGHTER_STATUS_JUMP_FLAG_GIMMICK_SPRING_JUMP_FROM_RING);
-       
-        // Whatever the fuck this is
-        //0xe1ba0(false, true);
-        
-        // Sound
-        if macros::is_excute(fighter) {
-            macros::PLAY_SE(fighter, Hash40::new("se_mario_special_h03"));
-            macros::PLAY_SE(fighter, Hash40::new("se_mario_jump01"));
-        }
+        macros::SA_SET(fighter, *SITUATION_KIND_AIR);
+        macros::ATTACK(fighter, 0, 0, Hash40::new("top"), 5.0, 60, 100, 160, 0, 2.5, 0.0, 6.5, 0.0, None, None, None, 1.0, 0.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, true, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_A, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, effect, *ATTACK_SOUND_LEVEL_M, sound, *ATTACK_REGION_PUNCH);
+        macros::ATTACK(fighter, 1, 0, Hash40::new("top"), 5.0, 86, 100, 150, 0, 4.0, 0.0, 6.5, 5.0, None, None, None, 1.0, 0.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, true, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, effect, *ATTACK_SOUND_LEVEL_M, sound, *ATTACK_REGION_PUNCH);
+        macros::ATTACK(fighter, 2, 0, Hash40::new("top"), 5.0, 100, 100, 150, 0, 4.0, 0.0, 6.3, 9.2, None, None, None, 1.0, 0.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, true, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, effect, *ATTACK_SOUND_LEVEL_M, sound, *ATTACK_REGION_PUNCH);
+        AttackModule::set_no_damage_fly_smoke_all(fighter.module_accessor, true, false);
+    }
+    wait(fighter.lua_state_agent, 3.0);
+    if macros::is_excute(fighter) {
+        WorkModule::on_flag(fighter.module_accessor, *FIGHTER_STATUS_SUPER_JUMP_PUNCH_FLAG_MOVE_TRANS);
+    }
+    wait(fighter.lua_state_agent, 1.0);
+    if macros::is_excute(fighter) {
+        AttackModule::clear_all(fighter.module_accessor);
+        macros::ATTACK(fighter, 0, 0, Hash40::new("top"), 0.6, 60, 100, 180, 0, 3.0, 0.0, 6.5, 2.5, None, None, None, 1.0, 0.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, true, 0, 0.0, 2, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, effect, *ATTACK_SOUND_LEVEL_M, sound, *ATTACK_REGION_PUNCH);
+        macros::ATTACK(fighter, 1, 0, Hash40::new("top"), 0.6, 92, 100, 170, 0, 3.8, 0.0, 6.5, 8.0, None, None, None, 1.0, 0.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, true, 0, 0.0, 2, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, effect, *ATTACK_SOUND_LEVEL_M, sound, *ATTACK_REGION_PUNCH);
+        macros::ATTACK(fighter, 2, 0, Hash40::new("top"), 0.6, 60, 100, 110, 0, 3.0, 0.0, 11.5, 2.5, None, None, None, 1.0, 0.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, true, 0, 0.0, 2, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, effect, *ATTACK_SOUND_LEVEL_M, sound, *ATTACK_REGION_PUNCH);
+        macros::ATTACK(fighter, 3, 0, Hash40::new("top"), 0.6, 92, 100, 110, 0, 3.0, 0.0, 11.5, 8.5, None, None, None, 1.0, 0.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, true, 0, 0.0, 2, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, effect, *ATTACK_SOUND_LEVEL_M, sound, *ATTACK_REGION_PUNCH);
+        AttackModule::set_no_finish_camera(fighter.module_accessor, 0, true, false);
+        AttackModule::set_no_finish_camera(fighter.module_accessor, 1, true, false);
+        AttackModule::set_no_finish_camera(fighter.module_accessor, 2, true, false);
+        AttackModule::set_no_finish_camera(fighter.module_accessor, 3, true, false);
+        AttackModule::set_no_damage_fly_smoke_all(fighter.module_accessor, true, false);
+    }
+    frame(fighter.lua_state_agent, 13.0);
+    if macros::is_excute(fighter) {
+        notify_event_msc_cmd!(fighter, Hash40::new_raw(0x2127e37c07), GROUND_CLIFF_CHECK_KIND_ALWAYS_BOTH_SIDES);
+    }
+    frame(fighter.lua_state_agent, 17.0);
+    if sound == *COLLISION_SOUND_ATTR_COIN {
+        sound = *COLLISION_SOUND_ATTR_MARIO_COIN_LAST;
+    }
+    else {
+        sound = *COLLISION_SOUND_ATTR_MARIO_LOCAL_COIN_LAST;
+    }
+    if macros::is_excute(fighter) {
+        AttackModule::clear_all(fighter.module_accessor);
+        macros::ATTACK(fighter, 0, 0, Hash40::new("top"), 3.0, 60, 145, 0, 50, 9.0, 0.0, 11.5, 8.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, effect, *ATTACK_SOUND_LEVEL_L, sound, *ATTACK_REGION_PUNCH);
+        macros::ATTACK(fighter, 1, 0, Hash40::new("top"), 3.0, 60, 145, 0, 50, 9.0, 0.0, 11.5, 2.5, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, effect, *ATTACK_SOUND_LEVEL_L, sound, *ATTACK_REGION_PUNCH);
+    }
+    wait(fighter.lua_state_agent, 2.0);
+    if macros::is_excute(fighter) {
+        AttackModule::clear_all(fighter.module_accessor);
     }
 }
 
